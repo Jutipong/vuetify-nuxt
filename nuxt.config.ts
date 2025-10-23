3b5d32feac7f38bf73f9c84e0a066fb23afa0c8b
Jutipong.Dev <techcom.ec20@gmail.com>
2025-10-23 16:29:20 +0700

    feat: add initial index page with basic structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: false },
   compatibilityDate: '2025-07-15',
-  // devtools: { enabled: true }
+  modules: ['vuetify-nuxt-module']
 })
